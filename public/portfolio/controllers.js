7ee00c1c2c607b5aad8658f190b1e4f5e5f040e9
vishalchilate <vishalchilate3@gmail.com>
2015-05-18 09:33:32 -0600

    Update controllers.js

--- public/portfolio/controllers.js
+++ public/portfolio/controllers.js
@@ -93,7 +93,7 @@ app.controller('forecastController', ['$scope', '$resource', '$routeParams', 'ci
     
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily");
     
     $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     
